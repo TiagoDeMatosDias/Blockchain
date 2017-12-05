9138918b8fcdce94360b848d700fc4428cda3b95
poloport <poloport@users.noreply.github.com>
2017-12-05 22:19:18 +0100

    Added Genesys block and List

--- Blockchain/Blockchain/Program.cs
+++ Blockchain/Blockchain/Program.cs
@@ -11,19 +11,37 @@ namespace Blockchain
     {
         static void Main(string[] args)
         {
+            List<Block> Blockchain = new List<Block>();
             Block block = new Block();
-            block.init(0, DateTime.Now, "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", "0000000000000000000000000000000000000000000000000000000000000000");
-
-            Console.WriteLine("Index: " + block.Index);
-            Console.WriteLine("TimeStamp: " + block.Timestamp);
-            Console.WriteLine("Data: " + block.Data);
-            Console.WriteLine("Previous Hash: " + block.Previous_hash);
-            Console.WriteLine("Hash: " + block.Hash);
+            block.Genesys();
+            Blockchain.Add(block);
+                        
+            Console.WriteLine("Index: " + Blockchain[0].Index);
+            Console.WriteLine("TimeStamp: " + Blockchain[0].Timestamp);
+            Console.WriteLine("Data: " + Blockchain[0].Data);
+            Console.WriteLine("Previous Hash: " + Blockchain[0].Previous_hash);
+            Console.WriteLine("Hash: " + Blockchain[0].Hash);
             Console.ReadLine();
-            
+
+            for (int i = 0; i > 6; i++)
+            {
+                block.next_block(Blockchain[i]);
+                Blockchain.Add(block);
+                Console.WriteLine("Index: " + Blockchain[i].Index);
+                Console.WriteLine("TimeStamp: " + Blockchain[i].Timestamp);
+                Console.WriteLine("Data: " + Blockchain[i].Data);
+                Console.WriteLine("Previous Hash: " + Blockchain[i].Previous_hash);
+                Console.WriteLine("Hash: " + Blockchain[i].Hash);
+                Console.ReadLine();
+            }
+
 
 
         }
+
+        
+
+
     }
 
 
@@ -37,20 +55,33 @@ namespace Blockchain
 
         public static string GetHash(string input)
         {
-            HashAlgorithm algorithm = SHA256.Create(); 
+            HashAlgorithm algorithm = SHA256.Create();
             return BitConverter.ToString(algorithm.ComputeHash(Encoding.UTF8.GetBytes(input))).Replace("-", String.Empty);
         }
-        
+
         public void init(int index, DateTime timestamp, string data, string previous_hash)
         {
             this.index = index;
             this.timestamp = timestamp;
             this.data = data;
             this.previous_hash = previous_hash;
-            this.hash = GetHash(this.index.ToString()+this.timestamp+this.data+this.previous_hash);
+            this.hash = GetHash(this.index.ToString() + this.timestamp + this.data + this.previous_hash);
         }
 
-         public int Index
+        public Block Genesys()
+        {
+            init(0, DateTime.Now, "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", "0000000000000000000000000000000000000000000000000000000000000000");
+            return this;
+        }
+
+        public Block next_block(Block last_block)
+        {
+            init(last_block.Index + 1, DateTime.Now, "Block: " + (last_block.Index + 1), last_block.hash);
+            return this;
+        }
+
+
+        public int Index
         {
             get { return index; }
         }
@@ -78,5 +109,9 @@ namespace Blockchain
 
 
     }
+
+    
+        
+    
 
 }
